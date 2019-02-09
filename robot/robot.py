bcd41e4f1a0bc5172f83e7b8669a01cc5f26c778
Owen Mellema <owenmllm@gmail.com>
2019-02-08 23:19:48 -0600

    Implement execute

--- robot/robot.py
+++ robot/robot.py
@@ -16,4 +16,25 @@ def idle():
     THings to do as the robot is idling.
     '''
     pass #I haven't decided on anything yet ;)
+
+def execute(toExecute):
+    '''
+    Executes a list of commands formatted in the way described by incomingInstruction.py.
+    @param toExecute A list of properly formatted command dictionaries. 
+    '''
+
+    for i in toExecute:
+        if i['command'] == 'forward':
+            forward(i['rotations'])
+        elif i['command'] == 'backward':
+            backward(i['rotations'])
+        elif i['command'] == 'left':
+            left()
+        elif i['command'] == 'right':
+            right()
+        elif i['command'] == 'stop':
+            stop(i['seconds'])
+        else:
+            raise KeyError(f"Unrecognized command {i['command']}.")
     
+
