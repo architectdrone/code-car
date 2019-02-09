2974779e5a5b0d9ebb2d06f91f7672c8634215e3
Owen Mellema <owenmllm@gmail.com>
2019-02-09 07:07:19 -0600

    Add obstacle detection

--- randomRobot.py
+++ randomRobot.py
@@ -1,3 +1,28 @@
 #Purpose: simulate "playful" behavior, by using random movements and collision detection
 
 import random
+import robot
+import RPi.GPIO as GPIO
+
+CHECK_INCREMENT = 1.0 #When the robot checks to make sure it isn't too close to a wall.
+P25= 25
+P24= 24
+P23= 23
+P5 = 5
+
+#GPIO setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(P25, GPIO.IN)
+GPIO.setup(P24, GPIO.IN)
+GPIO.setup(P23, GPIO.IN)
+GPIO.setup(P5, GPIO.IN)
+
+def forwardCheck():
+    '''
+    Checks the sensors to see if we are close to an object.
+    @return True if we are too close, False if we are good.
+    '''
+    if GPIO.input(P25) or GPIO.input(P24) or GPIO.input(P23) or GPIO.input(P5): 
+        return True
+    return False
+
