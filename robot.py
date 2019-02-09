d0e2c23843fb7ec46a13bffb81eabd74da24b2ad
Owen Mellema <owenmllm@gmail.com>
2019-02-09 00:46:16 -0600

    Organize the file

--- robot.py
+++ robot.py
@@ -2,7 +2,8 @@
 import time
 import RPi.GPIO as GPIO
 
-#GPIO Globals
+#GLOBALS: You set this.
+#GPIO
 IN1 = 12
 IN2 = 13
 ENA = 6
@@ -17,53 +18,18 @@ BACKWARD = [IN2, IN3]
 LEFT = [IN4]
 RIGHT = [IN1]
 
-#Setup GPIO
+#Calibration
+SECONDS_PER_1_ROTATION = 3.2 #We need to do some calibration of this.
+SECONDS_FOR_QUARTER_TURN = 3.4 #Also needs calibration
+
+#SETUP GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(IN1, GPIO.OUT)
 GPIO.setup(IN2, GPIO.OUT)
 GPIO.setup(IN3, GPIO.OUT)
 GPIO.setup(IN4, GPIO.OUT)
 
-#Calibration
-SECONDS_PER_1_ROTATION = 3.2 #We need to do some calibration of this.
-SECONDS_FOR_QUARTER_TURN = 3.4 #Also needs calibration
-
-def _run(port):
-    '''
-    PRIVATE: Starts driving power to the given pin.
-    @param port The port to drive to.
-    '''
-    GPIO.output(port, GPIO.HIGH)
-
-def _halt(port):
-    '''
-    PRIVATE: Stops driving power to the given pin.
-    @param port The port to stop driving to.
-    '''
-    GPIO.output(port, GPIO.LOW)
-
-def _runForGivenTime(portList, time):
-    '''
-    PRIVATE: runs a list of ports, for a given amount of time, before stopping them.
-    @param portList A list of ints specifing ports to run.
-    @param time Amount of time to run the ports.
-    '''
-    for i in portList:
-        _run(i)
-    time.sleep(time)
-    for i in portList:
-        _halt(i)
-
-
-def _convertRotationsToSeconds(rotations):
-    '''
-    Converts a number of rotations into seconds, which can be used by the hardware.
-    @param rotations Rotations to convert.
-    @return Float number of seconds. 
-    '''
-    global SECONDS_PER_1_ROTATION
-    return rotations*SECONDS_PER_1_ROTATION
-
+#PUBLIC
 def forward(rotations):
     '''
     Causes the robot to go forward by the number of rotations specified.
@@ -102,3 +68,42 @@ def stop(seconds):
     @param seconds Number of seconds to stop.
     '''
     time.sleep(seconds)
+
+#PRIVATE
+
+def _run(port):
+    '''
+    PRIVATE: Starts driving power to the given pin.
+    @param port The port to drive to.
+    '''
+    GPIO.output(port, GPIO.HIGH)
+
+def _halt(port):
+    '''
+    PRIVATE: Stops driving power to the given pin.
+    @param port The port to stop driving to.
+    '''
+    GPIO.output(port, GPIO.LOW)
+
+def _runForGivenTime(portList, time):
+    '''
+    PRIVATE: runs a list of ports, for a given amount of time, before stopping them.
+    @param portList A list of ints specifing ports to run.
+    @param time Amount of time to run the ports.
+    '''
+    for i in portList:
+        _run(i)
+    time.sleep(time)
+    for i in portList:
+        _halt(i)
+
+
+def _convertRotationsToSeconds(rotations):
+    '''
+    Converts a number of rotations into seconds, which can be used by the hardware.
+    @param rotations Rotations to convert.
+    @return Float number of seconds. 
+    '''
+    global SECONDS_PER_1_ROTATION
+    return rotations*SECONDS_PER_1_ROTATION
+
